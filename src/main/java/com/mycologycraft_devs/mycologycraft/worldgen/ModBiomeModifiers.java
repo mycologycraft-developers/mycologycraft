0ebb1398c603eead717fdb1f907ec27d128bfc77
CodedBeastVirum <40578187+CodedBeastVirum@users.noreply.github.com>
2025-11-26 21:53:38 +0000

    Pushing my changes before calling it a night needs a lot of cleanup, but this currently has a worldgen modifier that spawns a lava lake with bone in place of the lava and example blocks replacing the barrier
    This is a complete mess and needs cleanup
    
    im gonna wanna look at modifying the barrier possibly for the mycelium, or writing a custom function
    
    The following file will be good reference for how to do this:
    ~build\moddev\artifacts\neoforge-21.0.167-merged.jar!\net\minecraft\world\level\levelgen\feature\LakeFeature.class

--- src/main/java/com/mycologycraft_devs/mycologycraft/worldgen/ModBiomeModifiers.java
+++ src/main/java/com/mycologycraft_devs/mycologycraft/worldgen/ModBiomeModifiers.java
@@ -2,17 +2,31 @@ package com.mycologycraft_devs.mycologycraft.worldgen;
 
 import com.mycologycraft_devs.mycologycraft.MycologyCraft;
 
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.common.world.BiomeModifiers;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 public class ModBiomeModifiers {
+
+	public static final ResourceKey<BiomeModifier> OVERWORLD_LAVA_LAKE_MAGMA_MYCELIUM = registerKey("magma_mycelium_lava_lake");
+
 	public static void bootstrap(BootstrapContext<BiomeModifier> context) {
 		var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
 		var biomes = context.lookup(Registries.BIOME);
+
+		context.register(OVERWORLD_LAVA_LAKE_MAGMA_MYCELIUM, new BiomeModifiers.AddFeaturesBiomeModifier(
+				biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+				HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.MAGMA_MYCELIUM_LAVA_LAKE_KEY)),
+				GenerationStep.Decoration.LAKES
+		));
 	}
 
 	public static ResourceKey<BiomeModifier> registerKey(String name) {
--- src/main/java/com/mycologycraft_devs/mycologycraft/worldgen/ModConfiguredFeatures.java
+++ src/main/java/com/mycologycraft_devs/mycologycraft/worldgen/ModConfiguredFeatures.java
@@ -2,18 +2,28 @@ package com.mycologycraft_devs.mycologycraft.worldgen;
 
 import com.mycologycraft_devs.mycologycraft.MycologyCraft;
 
+import com.mycologycraft_devs.mycologycraft.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.LakeFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
 public class ModConfiguredFeatures {
+
+	public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_LAVA_LAKE_MAGMA_MYCELIUM = registerKey("magma_mycelium_lava_lake");
+
 	public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
-		
+		register(context, OVERWORLD_LAVA_LAKE_MAGMA_MYCELIUM, Feature.LAKE,
+				new LakeFeature.Configuration(BlockStateProvider.simple(Blocks.LAVA.defaultBlockState()), BlockStateProvider.simple(ModBlocks.EXAMPLE_BLOCK.get())));
 	}
+
+
 
 	public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
 		return ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.fromNamespaceAndPath(MycologyCraft.MODID, name));
