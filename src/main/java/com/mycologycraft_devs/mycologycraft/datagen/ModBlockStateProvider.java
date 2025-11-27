da48a071210cf8a620305cfad355b3ab8252cdec
Cassandra Hollinshead <cassandrahollinshead@gmail.com>
2025-11-27 00:17:19 +0000

    generalise multiblock mushroom block gen

--- src/main/java/com/mycologycraft_devs/mycologycraft/datagen/ModBlockStateProvider.java
+++ src/main/java/com/mycologycraft_devs/mycologycraft/datagen/ModBlockStateProvider.java
@@ -1,7 +1,6 @@
 package com.mycologycraft_devs.mycologycraft.datagen;
 
 
-import com.mycologycraft_devs.mycologycraft.MycologyCraft;
 import com.mycologycraft_devs.mycologycraft.block.ModBlocks;
 
 import net.minecraft.data.PackOutput;
@@ -25,15 +24,30 @@ public class ModBlockStateProvider extends BlockStateProvider {
     protected void registerStatesAndModels() {
       blockWithItem(ModBlocks.EXAMPLE_BLOCK);
 
-    	blockItem(ModBlocks.EXAMPLE_MUSHROOM_STEM, "_inventory");
-    	blockItem(ModBlocks.EXAMPLE_MUSHROOM_BLOCK, "_inventory");
-
-			models().singleTexture("example_mushroom_stem", ResourceLocation.parse("minecraft:block/template_single_face"), ResourceLocation.fromNamespaceAndPath(MycologyCraft.MODID, "block/example_mushroom_stem"));
-			models().singleTexture("example_mushroom_block", ResourceLocation.parse("minecraft:block/template_single_face"), ResourceLocation.fromNamespaceAndPath(MycologyCraft.MODID, "block/example_mushroom_block"));
-			models().singleTexture("example_mushroom_block_inside", ResourceLocation.parse("minecraft:block/template_single_face"), ResourceLocation.fromNamespaceAndPath(MycologyCraft.MODID, "block/example_mushroom_block_inside"));
-			models().cubeAll("example_mushroom_stem_inventory", ResourceLocation.fromNamespaceAndPath(MycologyCraft.MODID, "block/example_mushroom_stem"));
-			models().cubeAll("example_mushroom_block_inventory", ResourceLocation.fromNamespaceAndPath(MycologyCraft.MODID, "block/example_mushroom_block"));
+			bigMushroomBlocks(ModBlocks.EXAMPLE_MUSHROOM_STEM, ModBlocks.EXAMPLE_MUSHROOM_BLOCK);
     }
+
+		private void bigMushroomBlocks(DeferredBlock<?> stemBlock, DeferredBlock<?> capBlock) {
+			String stemModelName = stemBlock.getId().getPath();
+			String capModelName = capBlock.getId().getPath();
+			String capInsideModelName = capBlock.getId().getPath() + "_inside";
+			String stemInventoryModelName = stemBlock.getId().getPath() + "_inventory";
+			String capInventoryModelName = capBlock.getId().getPath() + "_inventory";
+
+			ResourceLocation parentTexture = mcLoc("block/template_single_face");
+			ResourceLocation stemTexture = blockTexture(stemBlock.get());
+			ResourceLocation capTexture = blockTexture(capBlock.get());
+			ResourceLocation insideTexture = DataGenHelper.asResource("block/" + capInsideModelName);
+
+			blockItem(stemBlock, "_inventory");
+			blockItem(capBlock, "_inventory");
+
+			models().singleTexture(stemModelName, parentTexture, stemTexture);
+			models().singleTexture(capModelName, parentTexture, capTexture);
+			models().singleTexture(capInsideModelName, parentTexture, insideTexture);
+			models().cubeAll(stemInventoryModelName, stemTexture);
+			models().cubeAll(capInventoryModelName, capTexture);
+		}
 
     //registers a cube block with an item
     private void blockWithItem(DeferredBlock<?> deferredBlock) {
